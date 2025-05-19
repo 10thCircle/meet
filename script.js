9078d28c88fd5dcc00cdd9cc57d960494984a45b
JacknS2520 <jackelly0221@gmail.com>
2025-05-19 16:50:48 -0500

    Hamberger Added

--- script.js
+++ script.js
@@ -231,3 +231,20 @@
         })
         .catch(error => console.error('Error checking role:', error));
 }
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  });
+});
+
